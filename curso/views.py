7a36b75c57c5e94bd57913846eabfa895aaf84c6
Joao Pedro <joaopedrojp20083@gmail.com>
2024-03-28 17:30:16 -0300

    Modifiquei os templates para mostrar os posts da database.

--- curso/views.py
+++ curso/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Curso
 
-# Create your views here.
+def curso_list(request):
+    cursos = Curso.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'curso/curso_list.html', {'cursos': cursos})
